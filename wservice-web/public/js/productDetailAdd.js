6fba3b7d4ab23aaf8f345e375cd630466af89a7e
ruanxiaoyu <ruanxiaoyu_520@163.com>
2015-10-19 16:34:51 +0800

    a

---
+++ wservice-web/public/js/productDetailAdd.js
@@ -0,0 +1,13 @@
+/**
+ * Created by rxy on 15/10/19.
+ */
+
+
+
+var script=document.createElement("script");
+script.type="text/javascript";
+script.src="../vendor/jQuery/jQuery-2.1.4.min.js"
+
+var bigbang= function(){
+    alert("111111");
+}
