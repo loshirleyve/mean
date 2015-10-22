31840888fa58e7d5229bf47c3e204a7516e99682
ruanxiaoyu <ruanxiaoyu_520@163.com>
2015-10-22 10:11:51 +0800

    添加航

--- wservice-web/public/js/productDetailAdd.js
+++ wservice-web/public/js/productDetailAdd.js
@@ -8,7 +8,34 @@ var script=document.createElement("script");
 script.type="text/javascript";
 script.src="../vendor/jQuery/jQuery-2.1.4.min.js"
 
-var addrow= function(){
-    $("#table1").append("<tr><td colspan='8'>"+fsfsfsfsfs+"</td><tr>");
-    alert("--------");
+var pos=0;
+function whichElement(e)
+{
+    var targ
+    if(!e) var e=window.event
+    if(e.target) targ= e.target
+    else if(e.srcElement)targ= e.srcElement
+    if(targ.nodeType==3)
+    targ=targ.parentNode
+    if(targ.targName="TD")
+    {
+        pos=targ.parentNode.rowIndex+1
+    }
+    else if(targ.tagName=="INPUT")
+    {
+
+    }
+    else
+    {
+        pos=0
+    }
+}
+
+function addRow()
+{
+    var x=document.getElementById("myTable").insertRow(pos)
+    var y= x.insertCell(0)
+    var z= x.insertCell(1)
+    y.innerHTML=document.getElementById("cell1").value
+    z.innerHTML=docuemnt.getElementById("cell2").value
 }
