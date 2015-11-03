bfdebc64848a1f64498e7643d6e723305a93e9b4
CKHuang <ckhuang326@icloud.com>
2015-11-03 21:06:58 +0800

    使用多个iframe控制多个子窗口，未完善：tab顺序，左侧菜单栏定位，消息提示，子窗口调用父窗口方法

--- wservice-web/public/js/app.js
+++ wservice-web/public/js/app.js
@@ -5,13 +5,46 @@
 'use strict';
 
 angular.module('wsweb', ['ngRoute', 'wsweb.service'])
-    .controller('launchCtrl', ['$scope', 'Menus', 'Session'
-        , function ($scope, Menus, Session) {
+    .controller('launchCtrl', ['$scope', 'Menus', 'Session', 'menuService', 'navigationMaster',
+        function ($scope, Menus, Session, menuService, navigationMaster) {
+
+            this.navigateTo = function (menuNo) {
+                menuService.navigateTo(menuNo);
+            }
+
+            this.changeTab = function (menuNo) {
+                navigationMaster.navigateTo(menuNo);
+            }
+
+            this.closeWindow = function (menuNo,$event) {
+                if ($event.stopPropagation) {
+                    $event.stopPropagation();
+                }
+                navigationMaster.closeWindow(menuNo);
+            }
+
+            Menus.query({userId: '10086', instId: '2'}).then(function (response) {
+                navigationMaster.init(response.data);
+                $scope.menus = response.data;
+                $scope.singleWindow = navigationMaster.singleWindow;
+                $scope.subWindows = navigationMaster.subWindows;
+                // 如果获取界面绘制完成的事件？
+                setTimeout(function() {
+                    $scope.subWindows.forEach(function(win) {
+                        $("#" + win.sid).load(function () {
+                            $("#" + win.sid).contents().find("body").attr("onclick",
+                                "window.parent.document.body.click();");
+                        });
+                    });
+                },300);
+            });
+
             Session.load().then(function (response) {
                 $scope.session = response.data;
             }, function (response) {
                 // 查询登录信息错误，跳转到登录界面
                 location.href = '/login';
             });
+
         }]);
 
--- wservice-web/public/js/navigation.js
+++ wservice-web/public/js/navigation.js
@@ -7,24 +7,9 @@
 'use strict';
 
 angular.module('wsweb')
-    .controller('navigationCtrl', function ($scope, Menus) {
-
-        this.menus = {};
-        // 初始化菜单，映射no跟url
-        this.initMenus = function (mns) {
-            var self = this;
-            mns.forEach(function(menu) {
-                if (menu.navigationItems) {
-                    menu.navigationItems.forEach(function(item) {
-                        self.menus[item.no] = item.mainurl;
-                    });
-                }
-            });
-        }
-
+    .service('menuService',function(navigationMaster) {
         // 导航到指定menuNo界面
         this.navigateTo = function (menuNo) {
-            var self = this;
             var checkElement = $("#mno_" + menuNo);
             var ul = checkElement.parent().parent().prev();
             if (!ul.is(":visible")) {
@@ -32,27 +17,149 @@ angular.module('wsweb')
             }
             var parentNode = checkElement.parent();
             parentNode.parent().children().removeClass('active');
+            navigationMaster.navigateTo(menuNo);
             setTimeout(function () {
                 parentNode.addClass('active');
-                $("#mainiframe", parent.document.body).attr("src", self.menus[menuNo]);
             }, 300);
         }
-        // 将iframe body的点击事件传递回主界面
-        $("#mainiframe").load(function () {
-            $("#mainiframe").contents().find("body").attr("onclick",
-                "window.parent.document.body.click();");
-        });
-        var self = this;
-        Menus.query({userId: '10086', instId: '2'}).then(function (response) {
-            var mns = response.data;
-            self.initMenus(mns);
-            $scope.menus = mns;
-        }, function (response) {
-            location.href = '/login';
-        });
     })
     .factory('navigationMaster', function () {
-        var navigationMaster = {};
+
+        var navigationMaster = {
+            openedNums:0,
+            limitNums:3,
+            increaseId:0,
+            currentFocus:undefined,
+            subWindows:[],
+            menus:{}
+        };
+
+        var SubWindow = function() {
+            this.sid = "sub_"+(navigationMaster.increaseId++);
+
+        }
+        /**
+         * 子窗口加载界面
+         * @param name
+         * @param url
+         */
+        SubWindow.prototype.load = function(menu) {
+            this.name = menu.name;
+            this.url = menu.mainurl;
+            this.menuNo = menu.no;
+            this.isOpen = true;
+            $("#"+this.sid, parent.document.body).attr("src", this.url);
+            this.focus();
+        }
+
+        SubWindow.prototype.focus = function() {
+            $("#"+this.sid).parent().children().hide();
+            $("#"+this.sid).show();
+            $("#"+this.sid+'_li').parent().children().removeClass('active');
+            $("#"+this.sid+'_li').show();
+            $("#"+this.sid+'_li').addClass('active');
+        }
+
+        SubWindow.prototype.close = function() {
+            this.isOpen = false;
+            $("#"+this.sid).hide();
+            $("#"+this.sid, parent.document.body).attr("src", "");
+            $("#"+this.sid+'_li').hide();
+
+            this.name = undefined;
+            this.url = undefined;
+            this.menuNo = undefined;
+        }
+        /**
+         * 初始化工程
+         * @param menus 菜单数据
+         */
+        navigationMaster.init = function(menus) {
+            if (this.subWindows.length > 0) {
+                throw new Error('已经初始化过navigationMaster了');
+            }
+            this.singleWindow = (this.limitNums == 1);
+            var self = this;
+            menus.forEach(function(menu) {
+                if (menu.navigationItems) {
+                    menu.navigationItems.forEach(function(item) {
+                        self.menus[item.no] = item;
+                    });
+                }
+            });
+            // 生成subWindow对象
+            for (var i = 0;i < this.limitNums;i++) {
+                this.subWindows.push(new SubWindow())
+            }
+        }
+        /**
+         * 导航到指定menuNo界面
+         * @param menuNo
+         */
+        navigationMaster.navigateTo = function (menuNo) {
+            var menu = this.menus[menuNo];
+            var self = this;
+            // 如果已经打开，则定位到tab
+            var isOpened = false;
+            var keepGoing = true;
+            this.subWindows.forEach(function(win) {
+                if (keepGoing && win.menuNo == menuNo) {
+                    isOpened = true;
+                    self.currentFocus = win;
+                    keepGoing = false;
+                }
+            });
+            if (isOpened) {
+                self.currentFocus.focus();
+                return;
+            }
+
+            // 如果是单窗口模式
+            if (this.singleWindow) {
+                if (!this.currentFocus) {
+                    this.currentFocus = this.subWindows[0];
+                }
+                this.currentFocus.load(menu);
+            } else if (this.openedNums < this.limitNums){   // 如果还没超过显示tab数
+                keepGoing = true;
+                this.subWindows.forEach(function(win){
+                    if (keepGoing && !win.name) {
+                        self.currentFocus = win;
+                        keepGoing = false;
+                    }
+                });
+                this.currentFocus.load(menu);
+                this.openedNums++;
+            } else {
+                // 警告
+            }
+        }
+
+        navigationMaster.closeWindow = function(menuNo) {
+            var keepGoing = true;
+            this.subWindows.forEach(function(win){
+                if (keepGoing && win.menuNo ==  menuNo) {
+                    win.close();
+                    navigationMaster.openedNums--;
+                    keepGoing = false;
+                }
+            });
+            if (this.currentFocus
+                && !this.currentFocus.menuNo) {
+                this.currentFocus = undefined;
+                keepGoing = true;
+                var self =this;
+                this.subWindows.forEach(function(win){
+                    if (keepGoing && win.menuNo) {
+                        self.currentFocus = win;
+                        keepGoing = false;
+                    }
+                });
+                if (this.currentFocus) {
+                    this.currentFocus.focus();
+                }
+            }
+        }
 
         return navigationMaster;
     });
