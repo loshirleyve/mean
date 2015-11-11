570eee22ebc281cd37a7d27adcff09472acb6823
chenkun <1192683199@qq.com>
2015-11-11 14:36:51 +0800

    ck

---
+++ wservice-web/router/inst.js
@@ -0,0 +1,20 @@
+/*!
+ * mars
+ * Copyright(c) 2015 Leon Huang
+ * MIT Licensed
+ */
+
+'use strict';
+
+var express = require("express");
+var router = express.Router();
+
+/**
+ * 订单列表路由
+ */
+router.get("/", function (req, res, next) {
+    res.render("app/inst/inst",{
+    });
+});
+
+module.exports = router;
