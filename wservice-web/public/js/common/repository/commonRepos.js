c4f93a328e62f12b1a61c40ab70eeb332fc85c86
CKHuang <ckhuang326@icloud.com>
2015-11-17 18:27:14 +0800

    增加一些常用Repos

--- wservice-web/public/js/common/repository/commonRepos.js
+++ wservice-web/public/js/common/repository/commonRepos.js
@@ -4,7 +4,57 @@
  * MIT Licensed
  */
 
-angular.module("wservice.common.repository.common",['ui.neptune.service.repository'])
-.factory("QueryCtrlCode", function (nptRepository) {
-    return nptRepository("QueryMdCtrlcode");
-})
+angular.module("wservice.common.repository.common",
+    ['ui.neptune.service.repository', "ui.neptune.service.session"])
+    .factory("QueryCtrlCode", function (nptRepository) {
+        return nptRepository("QueryMdCtrlcode");
+    }).factory("QueryImageByUserLevel", function (nptRepository, nptSessionManager) {
+        return nptRepository("QueryFile").params({
+            "userid": nptSessionManager.getSession.getUser().id,
+            "level": "user",
+            "instid": nptSessionManager.getSession.getUser().inst.id,
+            "filetype": "image"
+        }).addRequestInterceptor(function (request) {
+            return request;
+        });
+    }).factory("OrgListBySelectTree", function (nptRepository,nptSessionManager) {
+        function builderOrgTreeNode(nodes, data) {
+            if (data) {
+                nodes.nodes = [];
+                for (var i = 0; i < data.length; i++) {
+                    var node = {
+                        id: data[i]["id"],
+                        title: data[i]["name"]
+                    };
+                    builderOrgTreeNode(node, data[i].children);
+                    nodes.nodes.push(node);
+                }
+            }
+        }
+
+        return nptRepository("queryOrgTree").params({
+            "instid": nptSessionManager.getSession.getUser().inst.id,
+            "dimtype": "hr"
+        }).addResponseInterceptor(function (response) {
+            var orgNodes = [{
+                id: response.data.id,
+                title: response.data.simplename
+            }];
+            builderOrgTreeNode(orgNodes[0], response.data.children);
+            return orgNodes;
+        });
+
+    })
+    .factory("UserListBySelectTree", function (nptRepository) {
+        return nptRepository("queryUsersByOrgid").addRequestInterceptor(function (request) {
+            if (request.params.id) {
+                request.params = {
+                    orgid: request.params.id
+                };
+            }
+            return request;
+        });
+    })
+    .factory("QueryUserInfoById", function (nptRepository) {
+        return nptRepository("QueryUserInfoById");
+    });
--- wservice-web/router/api/session.js
+++ wservice-web/router/api/session.js
@@ -5,20 +5,29 @@
  */
 
 'use strict';
-module.exports = function(router) {
-    router.all('/session',function(req, res, next) {
+module.exports = function (router) {
+    router.all('/session', function (req, res, next) {
         var session = {};
         session.user = {
             id: 1,
-            name:'廖志祥',
-            iconid:1,
-            iconPath:'/vendor/adminlte/img/user2-160x160.jpg'
-        };
-        session.inst = {
-            id:1,
-            shotName:'时代共赢',
-            fullName:'深圳时代共赢企业服务有限公司',
-            pingyin:'SDGY'
+            name: '廖志祥',
+            iconid: 1,
+            iconPath: '/vendor/adminlte/img/user2-160x160.jpg',
+            inst: {
+                id: 1,
+                shotName: '时代共赢',
+                fullName: '深圳时代共赢企业服务有限公司',
+                pingyin: 'SDGY'
+            },
+            insts: [
+                {
+                    id: 1,
+                    shotName: '时代共赢',
+                    fullName: '深圳时代共赢企业服务有限公司',
+                    pingyin: 'SDGY'
+                }
+            ],
+            roles: []
         };
         res.json(session);
     });
