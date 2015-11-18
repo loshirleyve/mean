318c119b659e8db37cd7340b21c9e76df4465ca3
Administrator <2634582638@qq.com>
2015-11-18 14:53:04 +0800

    1

--- wservice-web/public/js/common/repository/commonRepos.js
+++ wservice-web/public/js/common/repository/commonRepos.js
@@ -7,4 +7,4 @@
 angular.module("wservice.common.repository.common",['ui.neptune.service.repository'])
 .factory("QueryCtrlCode", function (nptRepository) {
     return nptRepository("QueryMdCtrlcode");
-})
+});
