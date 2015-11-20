285bc24926937a75469d7c62d3577f7c2e7af3c2
CKHuang <ckhuang326@icloud.com>
2015-11-20 14:24:42 +0800

    迁移demo路由跟视图的文件结构

---
+++ wservice-web/middleware/biz/demo.js
@@ -0,0 +1,14 @@
+/*!
+ * mars
+ * Copyright(c) 2015 Leon Huang
+ * MIT Licensed
+ */
+
+'use strict';
+
+module.exports = function () {
+
+    return function (req, res, next) {
+        res.render("biz/demo/demo");
+    };
+};
