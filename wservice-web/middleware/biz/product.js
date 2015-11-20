0b33754c008af164abfe712bf12a94d138883604
ruanxiaoyu <ruanxiaoyu_520@163.com>
2015-11-20 16:46:54 +0800

    修改product

---
+++ wservice-web/middleware/biz/product.js
@@ -0,0 +1,12 @@
+/**
+ * Created by rxy on 15/11/3.
+ */
+
+'use strict';
+
+module.exports = function () {
+
+    return function (req, res, next) {
+        res.render("biz/product/product");
+    };
+};
--- wservice-web/router/index.js
+++ wservice-web/router/index.js
@@ -7,20 +7,12 @@
 'use strict';
 
 var main = require("./main");
-var receivable = require("./receivable");
-var product = require("./product");
 var file = require("./file");
 
 module.exports = function (app) {
 
     //使用主路由器
     app.use("/", main);
-
-    //收款列表
-    app.use("/receivable", receivable);
-
-    //产品列表
-    app.use("/product", product);
 
     //文件
     app.use("/file", file);
