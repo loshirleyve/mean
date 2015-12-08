773b8454d76565c35f48f64ce5b94febd738e4c5
leon <leon@yun9.com>
2015-12-08 16:32:29 +0800

    初始化mobile项目

---
+++ wservice-mobile-web/middleware/index.js
@@ -0,0 +1,29 @@
+/**
+ * Created by leon on 15/11/19.
+ */
+var debug = require("debug")("y9-wservice-mobile-web-middleware-loader");
+var fs = require('fs');
+var path = require('path');
+
+module.exports = function (app) {
+    var files = fs.readdirSync(__dirname + "");
+
+    files.forEach(function (file) {
+        var fileStat = fs.statSync(path.join(__dirname, file));
+
+        if (fileStat.isDirectory()) {
+            debug("加载目录:/" + file);
+            var router = require(path.join(__dirname, file));
+            var routerFn = router();
+            var paths = ["/" + file];
+
+            if (router.otherPaths) {
+                paths = paths.concat(router.otherPaths);
+            }
+
+            paths.forEach(function (pathValue) {
+                app.use(pathValue, routerFn);
+            });
+        }
+    });
+};
