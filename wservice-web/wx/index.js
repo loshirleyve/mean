42c556387c5e2e93b83c61e748849683388cb9de
leon <leon@yun9.com>
2015-12-18 16:14:13 +0800

    1

--- wservice-web/wx/index.js
+++ wservice-web/wx/index.js
@@ -3,6 +3,8 @@
  */
 
 var wxApi = require("y9-wx-api");
+var superagent = require("superagent");
+sha1 = require('sha1');
 var debug = require("debug")("y9-wservice-wx-gateway")
 module.exports = function (app) {
 
@@ -10,12 +12,56 @@ module.exports = function (app) {
     app.get("/wx/gateway", function (req, res, next) {
         debug("微信接入验证网关.", req.query);
 
-        var sigState = wxApi.checkSignature(req.query)
+        getToken("wx3f12d479b0082446", "eb61f0db8aa0403d30e9f9f3fe569af0", function (response) {
+            if (response && response.access_token) {
+                var sigState = checkSignature(req, response.access_token);
 
-        if (sigState) {
-            res.send(200, req.query.echostr);
-        } else {
-            res.send(200, 'fail');
-        }
+                if (sigState) {
+                    res.send(200, req.query.echostr);
+                } else {
+                    res.send(200, 'fail');
+                }
+            } else {
+                res.send(200, 'fail');
+            }
+        });
+
+
     });
+
+    function getToken(clientID, clientSecret, done) {
+        var url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=" + clientID + "&secret=" + clientSecret;
+
+        superagent.get(url)
+            .end(function (err, res) {
+                var result = res.body;
+                if (done) {
+                    done(result);
+                }
+            });
+    }
+
+    function checkSignature(req, token) {
+
+        // 获取校验参数
+        var signature = req.query.signature,
+            timestamp = req.query.timestamp,
+            nonce = req.query.nonce,
+            echostr = req.query.echostr;
+
+        // 按照字典排序
+        var array = [token, timestamp, nonce];
+        array.sort();
+
+        // 连接
+        var str = sha1(array.join(""));
+
+        debug("计算后的签名:" + str);
+        // 对比签名
+        if (str == signature) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 };
