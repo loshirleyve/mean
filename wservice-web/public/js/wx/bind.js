02bff0a38b59914892d2d3bd2a753a6d4eaf1243
leon <leon@yun9.com>
2015-12-21 16:23:10 +0800

    11

--- wservice-web/public/js/wx/bind.js
+++ wservice-web/public/js/wx/bind.js
@@ -2,18 +2,31 @@
  * Created by leon on 15/12/21.
  */
 
-angular.module("BindWxApp", ["ui.neptune", "ngRoute"])
+angular.module("BindWxApp", ["ui.neptune", "ngRoute", "ui-notification"])
     .config(function ($routeProvider) {
         $routeProvider.when("/", {
             templateUrl: "bind.html",
             controller: "BindWxController as vm"
+        }).when("/failed", {
+            templateUrl: "failed.html",
+            controller: "FailedController as vm"
         }).otherwise({
             redirectTo: "/"
         })
     })
-    .controller("BindWxController", function () {
+    .controller("BindWxController", function ($location, Notification) {
         var vm = this;
         //从页面读取微信数据
-        var temp = $("#wxprofile").html();
-        vm.wxProfile = angular.fromJson(temp);
+        vm.wxProfile = angular.fromJson($("#wxprofile").html());
+
+        if (!vm.wxProfile) {
+            $location.path("/failed")
+            Notification.error({
+                title: "绑定失败",
+                message: "无法获取微信认证信息.",
+                delay: 5000
+            })
+        }
+    }).controller("FailedController", function () {
+
     });
