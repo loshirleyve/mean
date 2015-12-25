25ba7d6f5a823cbde2ca10caee62502ff604feef
Administrator <2634582638@qq.com>
2015-12-25 17:40:17 +0800

    loginAutoSelectInst

--- wservice-web/middleware/inst/select.js
+++ wservice-web/middleware/inst/select.js
@@ -7,7 +7,11 @@ module.exports = function () {
         req.y9proxy.post("QueryCurrInstByUser").params({
             "userid": req.user.id
         }).launch(function (response) {
-            res.redirect("/system/inst/setup?instid=" + response.body.data.id);
+            if(response.body.data){
+                res.redirect("/system/inst/setup?instid=" + response.body.data.id);
+            }else{
+                res.render("inst/select");
+            }
         }, function (error) {
             res.render("inst/select");
         });
