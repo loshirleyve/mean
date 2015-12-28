af8314f2c97fa3e9a90e43e92468160be1a5f91e
CKHuang <ckhuang326@icloud.com>
2015-12-28 15:04:57 +0800

    1. 修改绑定成功后的返回

--- wservice-web/middleware/mobile/wx/bind.js
+++ wservice-web/middleware/mobile/wx/bind.js
@@ -6,7 +6,8 @@ module.exports = function () {
     return function (req, res, next) {
 
         res.render("mobile/wx/bind", {
-            "wxprofile": JSON.stringify(req.session.y9WxProfile)
+            "wxprofile": JSON.stringify(req.session.y9WxProfile),
+            "returnTo" : req.session.returnTo || "/auth/loginByWeixinClient"
         });
 
 
