ac4e41ac9b8b65a24e4a334cfcfd0ab46b97333a
Administrator <2634582638@qq.com>
2015-12-28 18:10:41 +0800

    apiDoc

--- wservice-web/middleware/api/test.js
+++ wservice-web/middleware/api/test.js
@@ -2,13 +2,13 @@
  * Created by leon on 15/12/28.
  */
 
-var service = require("y9-mars-service");
-var proxy = service.Proxy();
-
 exports = module.exports = function () {
     return function (req, res, next) {
-        //TODO 从req中获取请求参数
-        //
+        var service = require("y9-mars-service");
+        var proxy = service.Proxy();
+
+        var params = req.body;
+
         // 注册策略
         proxy.use("Y9", service.ProxyY9({
             token: "8fc50dd14a951318ca168e40a9fa1ec78d1110e058700c9affdbe6ab5eb6b931",
@@ -18,16 +18,15 @@ exports = module.exports = function () {
 
         proxy.action("ServiceName", {
             proxy: "Y9",
-            action: "com.yun9.ws.biz.service.QueryWorkOrdersService"
+            action: params.action
         });
 
         proxy.post("ServiceName")
-            .params({})
+            .params(params.data)
             .launch(function (response) {
-                //TODO 执行成功
                 res.send(response.body.data);
             }, function (error) {
-                res.send(error);
+                res.send(error.message);
             });
     }
 };
