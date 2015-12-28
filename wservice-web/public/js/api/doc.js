b01c473f58ab2bdf478a9b8937283053b73470b9
Administrator <2634582638@qq.com>
2015-12-28 15:01:24 +0800

    1

--- wservice-web/public/js/api/doc.js
+++ wservice-web/public/js/api/doc.js
@@ -23,12 +23,12 @@ angular.module("ServiceDocApp", ["ui.neptune", "ngRoute", "ui-notification"])
         $scope.query();
     }).filter("docDataFilter",function() {
         return function(input) {
-            if (!input) return input;
+            if (!input || input.length == 0) return "{}";
             var output = "{\r\n";
             angular.forEach(input,function(each) {
                 output += "      \""+ each.name +"\" : \"\",\r\n"
             });
-            output = output.substring(0,output.Length-1);
-            return output+"}";
+            output = output.substring(0,output.length - 3);
+            return output + "\r\n    }";
         };
     });
