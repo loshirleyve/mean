c7fb5cd26b356656465ac7b2983828f585f2944b
CKHuang <ckhuang326@icloud.com>
2015-12-29 17:08:08 +0800

    微信登录地址错误

--- wservice-web/wx/index.js
+++ wservice-web/wx/index.js
@@ -118,13 +118,13 @@ module.exports = function (app) {
         var message = req.weixin;
         var content = message.Content;
         if (content == "登录") {
-            res.reply("请点击<a href='http://www.yun9.com'>这里</a>进行登录");
+            res.reply("请点击<a href='http://www.yun9.com/auth/loginByWeixinClient'>这里</a>进行登录");
         }else if (content === '功能') {
             res.reply(getAllMenus());
         } else if (content === '基础信息') {
             res.reply(getBaseinfoMenus());
         } else if (content === '通用设置') {
-            res.reply(getSetupMenus());
+            res.reply(getSetupMenus());auth/loginByWeixinClient
         } else if (content === '业务中心') {
             res.reply(getBaseinfoMenus());
         } else {
