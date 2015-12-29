0ab9ea9a0fe6ba662333cfc64acff3637da26849
CKHuang <ckhuang326@icloud.com>
2015-12-29 16:29:08 +0800

    测试

--- wservice-web/middleware/mobile/wx/redirectMenu.js
+++ wservice-web/middleware/mobile/wx/redirectMenu.js
@@ -7,9 +7,6 @@
 module.exports = function () {
     return function (req, res, next) {
         var path = req.param('path');
-        res.statusCode = 302;
-        res.setHeader('Location', path);
-        res.setHeader('Content-Length', '0');
-        res.end();
+        require("../.."+path)(req,res,next);
     }
 };
