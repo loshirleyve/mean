6274e6e6ab0b8e0062c042577a57bb94cadde41b
Administrator <2634582638@qq.com>
2015-12-30 16:50:27 +0800

    apiDoc

--- wservice-web/public/js/api/doc.js
+++ wservice-web/public/js/api/doc.js
@@ -34,17 +34,18 @@ angular.module("ServiceDocApp", ["ui.neptune", "ngRoute", "ui-notification"])
                 "data":params
             };
             console.log(data);
-            return $http.post('/api/test',data).then(function(response) {
+            vm.result = $http.post('/api/test',data).then(function(response) {
                 action.inputs.paramsDefs.response = response.data;
             });
-        }
+            return result;
+        };
 
     }).filter("docDataFilter",function() {
         return function(input) {
-            if (!input || input.length == 0) return "{}";
+            if (!input || input.length === 0) return "{}";
             var output = "{\r\n";
             angular.forEach(input,function(each) {
-                output += "      \""+ each.name +"\" : \"\",\r\n"
+                output += "      \""+ each.name +"\" : \"\",\r\n";
             });
             output = output.substring(0,output.length - 3);
             return output + "\r\n    }";
