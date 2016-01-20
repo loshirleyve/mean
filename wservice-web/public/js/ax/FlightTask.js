b2dbf9d84c5354970c3aab4d06ba3c7bc230458e
ruanxiaoyu <ruanxiaoyu_520@163.com>
2016-01-20 16:37:05 +0800

    任列表

--- wservice-web/public/js/ax/FlightTask.js
+++ wservice-web/public/js/ax/FlightTask.js
@@ -26,25 +26,71 @@ angular.module("FlightTaskApp", ["ui.neptune", "wservice.common", "ngRoute"])
         })
     }).factory("GetTaskUrl", function (nptRepository) {
         return nptRepository("GetTaskUrl");
-    })
-    .controller("FlightTaskController", function ($routeParams, $location,$window, queryWorkorderList,GetTaskUrl, nptSessionManager, Notification) {
-        var vm = this;
-        //工单信息资源库
-        vm.workorderList = queryWorkorderList;
-        //查询任务
-        vm.query = function () {
-            vm.workorderList.post({})
-                .then(function (response) {
-                vm.model = response.data;
+    }).service("FlightTaskListQueryService", function (Notification, queryWorkorderList) {
+        var self = this;
+
+        self.flightTaskList = queryWorkorderList;
+
+        self.query = function (params) {
+            params = params || {};
+            self.flightTaskList.post(params).then(function (response) {
+                console.info(response.data);
             }, function (error) {
-                Notification.error({
-                    title: '获取任务列表出错',
-                    message: error.data.cause,
-                    replaceMessage: true,
-                    delay: 5000
-                });
+                Notification.error({title: '查询任务列表出现错误,请稍后再试.',message: error.data.cause, delay: 2000});
             });
         };
+
+        //建立待查询列表
+        self.queryList = [{
+            label: "全部",
+            type: "all",
+            callback: function () {
+                self.query();
+            }
+        }, {
+            label: "未开始",
+            type: "unstart",
+            callback: function () {
+                self.query({
+                    state: "unstart"
+                });
+            }
+        }, {
+            label: "处理中",
+            type: "inservice",
+            callback: function () {
+                self.query({
+                    state: "inservice"
+                });
+            }
+        }, {
+            label: "已完成",
+            type: "complete",
+            callback: function () {
+                self.query({
+                    state: "complete"
+                });
+            }
+        }];
+
+        //选择查询列表
+        self.selectQuery = function (query) {
+            if (query) {
+                self.currQuery = query;
+                if (query.callback) {
+                    query.callback();
+                }
+            }
+        };
+
+        //选择一个默认查询
+        self.selectQuery(self.queryList[0]);
+
+    })
+    .controller("FlightTaskController", function ($routeParams, $location,$window, FlightTaskListQueryService,GetTaskUrl, nptSessionManager, Notification) {
+        var vm = this;
+        //工单信息资源库
+        vm.queryService = FlightTaskListQueryService;
 
         vm.getUrl = function (params) {
             GetTaskUrl.post(params)
@@ -74,6 +120,5 @@ angular.module("FlightTaskApp", ["ui.neptune", "wservice.common", "ngRoute"])
             }
             vm.getUrl(params);
         };
-        vm.query();
 
     });
