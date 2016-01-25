39276b8f501fb8bf3df755626405cdc8307fc30c
Administrator <2634582638@qq.com>
2016-01-25 12:04:29 +0800

    order

--- wservice-web/public/js/mobile/myOrder.js
+++ wservice-web/public/js/mobile/myOrder.js
@@ -121,7 +121,7 @@ angular.module("MyOrderApp", ["ui.neptune", "ui-notification", "ngRoute"])
         vm.orderid = $routeParams.orderid;
         vm.orderDetail = queryOrderInfo;
         vm.orderDetail.post({"orderid":vm.orderid}).then(function(res){
-
+            vm.putImg(res.data);
         }, function(err){
             Notification.error({
                 message:err.data.cause,
@@ -138,6 +138,44 @@ angular.module("MyOrderApp", ["ui.neptune", "ui-notification", "ngRoute"])
         };
         vm.toPayInfo = function(orderid){
             $location.path('/payInfo/'+orderid);
+        };
+        var flag = false;
+        var imgno="";
+        vm.putImg = function(orderDetail){
+            for(var i=0; i<orderDetail.orderLogs.length; i++){
+                if(orderDetail.orderLogs[i].orderstate === orderDetail.order.state){
+                    orderDetail.orderLogs[i].imgno="1";
+                    flag = true;
+                }else if(orderDetail.orderLogs[i].orderstate !== orderDetail.order.state && flag === false){
+                    orderDetail.orderLogs[i].imgno="0";
+                }else{
+                    orderDetail.orderLogs[i].imgno="2";
+                }
+            }
+        };
+    }).filter("timeAgoFilter", function($filter){
+        return function(dateMillis){
+            var minute = 60 * 1000;
+            var hour = 60 * minute;
+            var day = 24 * hour;
+            var week = 7 * day;
+            var offset = new Date().getTime() - dateMillis;
+            var ago = "";
+            if(offset < minute){
+                ago = "刚刚";
+            }else if(offset < hour){
+                t = Math.floor(offset/minute);
+                ago = t + "分钟前";
+            }else if(offset < day){
+                t = Math.floor(offset/hour);
+                ago = t + "小时前";
+            }else if(offset < week){
+                t = Math.floor(offset/day);
+                ago = t + "天前";
+            }else{
+                ago = $filter("timestampFilter")(dateMillis);
+            }
+            return ago;
         }
     })
     .controller("OrderComplaintController", function($routeParams, queryOrderInfo, Notification, queryFileById, $location, AddComplain){
